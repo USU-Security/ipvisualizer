3fc34784de58c789a68da0507f40c175842e2322
A00283679 <A00283679@0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a>
2007-09-13 23:59:38 +0000

    mmm... function...
    git-svn-id: https://svn.usu.edu/repos/ipvisualizer@19 0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a

--- ipv-fun/test.py
+++ ipv-fun/test.py
@@ -1,6 +1,7 @@
 from bmp import BMPWrapper
 from ip_foo import *
 from socket import *
+import time
 
 debug = 0
 
@@ -22,32 +23,48 @@ def rev_netblock( num ):
 	y_outer = (num / 4096 ).__int__() % 26
 	return y_outer * 4096 + x_outer * 256 + y_inner * 16 + x_inner
 
-x = BMPWrapper('ayb.bmp')
-i = 0
+def draw_bmp( filename ):
+	x = BMPWrapper( filename )
+	i = 0
+	pixels = []
+	while i < x.biWidth*x.biHeight:
+		if x.rgbdata[i]:
+			address = fwd_netblock(i)
+			#address = i
+			# FIXME: separate r/g/b?
+			# FIXME: deal with intensity?
+			pixels.append(address)
+		i += 1
+	s = socket(AF_INET,SOCK_RAW,IPPROTO_RAW)
+	f = open('packets.hex','w')
+	for pixel in pixels:
+		ip_hdr = build_ip_header( dst = 0x817b0000 | pixel, ttl = 3, proto = 17 )
+		udp_hdr = build_udp_header( spt = 1234, dpt = 4321, len = 8 )
+		port = 4321
+		#try:
+		#	s.send( ip_hdr + udp_hdr )
+		#except:
+		#	print_ip(addr & 0x0000FFFF)
+		if debug:
+			f.write( ip_hdr + udp_hdr )
+		s.sendto( ip_hdr + udp_hdr, (get_ip(pixel),port) )
+	del x
 
-pixels = []
-while i < x.biWidth*x.biHeight:
-	if x.rgbdata[i]:
-		address = fwd_netblock(i)
-		#address = i
-		# FIXME: separate r/g/b?
-		# FIXME: deal with intensity?
-		pixels.append(address)
-	i += 1
+draw_bmp('ayb.bmp')
+draw_bmp('ayb.bmp')
+#draw_bmp('ayb.bmp')
 
-s = socket(AF_INET,SOCK_RAW,IPPROTO_RAW)
-f = open('packets.hex','w')
+time.sleep(10)
 
-for pixel in pixels:
-	ip_hdr = build_ip_header( dst = 0x817b0000 | pixel, ttl = 3, proto = 17 )
-	udp_hdr = build_udp_header( spt = 1234, dpt = 4321, len = 8 )
-	port = 4321
-	#try:
-	#	s.send( ip_hdr + udp_hdr )
-	#except:
-	#	print_ip(addr & 0x0000FFFF)
-	if debug:
-		f.write( ip_hdr + udp_hdr )
-	s.sendto( ip_hdr + udp_hdr, (get_ip(pixel),port) )
-	s.sendto( ip_hdr + udp_hdr, (get_ip(pixel),port) )
+draw_bmp('/home/esk/stickmiles.bmp')
+draw_bmp('/home/esk/stickmiles.bmp')
+#draw_bmp('/home/esk/stickmiles.bmp')
+
+time.sleep(10)
+
+draw_bmp('/home/esk/obey.bmp')
+draw_bmp('/home/esk/obey.bmp')
+#draw_bmp('/home/esk/obey.bmp')
+
+
 
