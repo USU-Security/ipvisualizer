f9bf20b169ea0e4e7ddf823ac0e82642c6ba4f5d
A00283679 <A00283679@0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a>
2007-09-26 22:07:50 +0000

    don't panic!
    git-svn-id: https://svn.usu.edu/repos/ipvisualizer@21 0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a

--- ipv-fun/test.py
+++ ipv-fun/test.py
@@ -3,7 +3,7 @@ from ip_foo import *
 from socket import *
 import time
 
-debug = 0
+debug = 1
 
 def get_ip( number ):
 	return "129.123.%d.%d" % (number%256,(number/256).__int__())
@@ -29,42 +29,77 @@ def draw_bmp( filename ):
 	pixels = []
 	while i < x.biWidth*x.biHeight:
 		if x.rgbdata[i]:
-			address = fwd_netblock(i)
+			a = x.rgbdata[i] >> 24 & 0xFF
+			r = x.rgbdata[i] >> 16 & 0xFF
+			g = x.rgbdata[i] >>  8 & 0xFF
+			b = x.rgbdata[i]       & 0xFF
+			pixel = fwd_netblock(i)
 			#address = i
 			# FIXME: separate r/g/b?
 			# FIXME: deal with intensity?
-			pixels.append(address)
+			red =   int( 4/255.0 * r )
+			green = int( 4/255.0 * g )
+			blue =  int( 4/255.0 * b )
+			if a:
+				pixels.append( (pixel,red,green,blue) )
 		i += 1
 	s = socket(AF_INET,SOCK_RAW,IPPROTO_RAW)
 	f = open('packets.hex','w')
+	port = 4321
+	P=0
+	R=1
+	G=2
+	B=3
 	for pixel in pixels:
-		ip_hdr = build_ip_header( dst = 0x817b0000 | pixel, ttl = 3, proto = 17 )
-		udp_hdr = build_udp_header( spt = 1234, dpt = 4321, len = 8 )
-		port = 4321
-		#try:
-		#	s.send( ip_hdr + udp_hdr )
-		#except:
-		#	print_ip(addr & 0x0000FFFF)
-		if debug:
-			f.write( ip_hdr + udp_hdr )
-		s.sendto( ip_hdr + udp_hdr, (get_ip(pixel),port) )
+		if( pixel[R] ):
+			udp_ip_hdr = build_ip_header( dst = 0x817b0000 | pixel[P], ttl = 3, proto = 17 )
+			udp_hdr = build_udp_header( spt = 1234, dpt = 4321, len = 8 )
+			if debug:
+				f.write( udp_ip_hdr + udp_hdr )
+			i = 0
+			while i < pixel[R] : 
+				s.sendto( udp_ip_hdr + udp_hdr, (get_ip(pixel[P]),port) )
+				i+=1
+		
+		if( pixel[G] ):
+			tcp_ip_hdr = build_ip_header( dst = 0x817b0000 | pixel[P], ttl = 3, proto = 6 )
+			tcp_hdr = build_tcp_header( spt = 1234, dpt = 4321, len = 0 )
+			if debug:
+				f.write( tcp_ip_hdr + tcp_hdr )
+			i=0
+			while i < pixel[G] : 
+				s.sendto( tcp_ip_hdr + tcp_hdr, (get_ip(pixel[P]),port) )
+				i+=1
+
+		if( pixel[B] ):
+			icmp_ip_hdr = build_ip_header( dst = 0x817b0000 | pixel[P], ttl = 3, proto = 1 )
+			icmp_hdr = build_icmp_header(type=8)
+			if debug:
+				f.write( icmp_ip_hdr + icmp_hdr )
+			i = 0
+			while i < pixel[B] : 
+				s.sendto( icmp_ip_hdr + icmp_hdr, (get_ip(pixel[P]),port) )
+				i+=1
+
 	del x
 
-draw_bmp('ayb.bmp')
-draw_bmp('ayb.bmp')
+#draw_bmp('ayb.bmp')
+#draw_bmp('ayb.bmp')
 #draw_bmp('ayb.bmp')
 
-time.sleep(10)
+#time.sleep(10)
 
-draw_bmp('/home/esk/stickmiles.bmp')
-draw_bmp('/home/esk/stickmiles.bmp')
+#draw_bmp('/home/esk/stickmiles.bmp')
+#draw_bmp('/home/esk/stickmiles.bmp')
 #draw_bmp('/home/esk/stickmiles.bmp')
 
-time.sleep(10)
+#time.sleep(10)
 
-draw_bmp('/home/esk/obey.bmp')
-draw_bmp('/home/esk/obey.bmp')
 #draw_bmp('/home/esk/obey.bmp')
+#draw_bmp('/home/esk/obey.bmp')
+#draw_bmp('/home/esk/obey.bmp')
+
+draw_bmp('/home/esk/panic.bmp')
 
 
 
