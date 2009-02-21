48402d0133847f0561cc032e235d0b16d9fa9c73
A00283679 <A00283679@0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a>
2009-02-21 01:11:30 +0000

    yay...
    git-svn-id: https://svn.usu.edu/repos/ipvisualizer@63 0f5e6fca-83d9-4d0a-8db4-3a67c99cac7a

--- ipv-fun/draw.py
+++ ipv-fun/draw.py
@@ -8,15 +8,23 @@ import sys
 import random
 import xkcd
 
-debug = 1
-
 # IP address of our UEN box
 src = 0xcd790082
 
+debug = 0
+#debug = 1
+
+P=0
+R=1
+G=2
+B=3
+
 random.seed()
 
+dims = { 'x':256, 'y':256 }
+
 def get_ip( number ):
-	return "129.123.%d.%d" % (int(number/256),number%256)
+	return "129.123.%d.%d" % ( int(number/dims['x']), number%dims['x'] )
 
 
 def fwd_netblock( num ):
@@ -29,9 +37,9 @@ def fwd_netblock( num ):
 def rev_netblock( num ):
 	x_inner = num % 16
 	y_inner = (num / 16).__int__() % 16
-	x_outer = (num / 256 ).__int__() % 26
-	y_outer = (num / 4096 ).__int__() % 26
-	return y_outer * 4096 + x_outer * 256 + y_inner * 16 + x_inner
+	x_outer = (num / 256 ).__int__() % 16
+	y_outer = (num / 4096 ).__int__() % 16
+	return y_outer * 4096 + y_inner * 256 + x_outer * 16 + x_inner
 
 def fwd_xkcd( num ):
 	return xkcd.forward[num]
@@ -40,7 +48,7 @@ def rev_xkcd( num ):
 	return xkcd.reverse[num]
 
 
-def get_rgb_from_bmp( filename ):
+def get_pixels_from_bmp( filename ):
 	x = BMPWrapper( filename )
 	i = 0
 	pixels = []
@@ -58,7 +66,7 @@ def get_rgb_from_bmp( filename ):
 			red =   int( max_brightness/255.0 * r )
 			green = int( max_brightness/255.0 * g )
 			blue =  int( max_brightness/255.0 * b )
-			if a:
+			if a and ( red or green or blue ):
 				pixels.append( (pixel,red,green,blue) )
 		i += 1
 	del x
@@ -71,10 +79,6 @@ def draw_pixels( pixels ):
 	s = socket(AF_INET,SOCK_RAW,IPPROTO_RAW)
 	f = open('packets.hex','w')
 	port = 4321
-	P=0
-	R=1
-	G=2
-	B=3
 	tcp_data="Random data for a TCP packet..."
 	for pixel in pixels:
 		if( pixel[R] ):
@@ -113,16 +117,24 @@ def draw_pixels( pixels ):
 			while i < pixel[B] : 
 				s.sendto( pkt, (get_ip(pixel[P]),port) )
 				i+=1
+def shift_pixels( pixels, x, y, wrap=True ):
+	newpixels = []
+	for pos, r, g, b in pixels:
+		new_pos_xy = fwd_netblock( pos ) + x + dims['x'] * y
+		if new_pos_xy < ( dims['x'] * dims['y'] ) or wrap:
+			new_pos = rev_netblock( new_pos_xy % ( dims['x'] * dims['y'] ) )
+			newpixels.append( ( new_pos, r, g, b, ) )
+	return newpixels
 
 def draw_bmp( filename ):
-	pixels = get_rgb_from_bmp(filename)
+	pixels = get_pixels_from_bmp(filename)
 	draw_pixels( pixels )
 
 
 if __name__ == "__main__":
 	print sys.argv
 	global fwd_map
-	fwd_map = fwd_netblock
+	fwd_map = fwd_xkcd
 	#time.sleep(10)
 	if len(sys.argv) > 1:
 		picture = sys.argv[1]
